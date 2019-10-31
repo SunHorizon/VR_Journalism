855638e0b472ab3afae3129a0c70bce90534ab89
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2019-10-31 19:23:53 -0400

    added graze

--- VRTest/Assets/Scripts/UI_Manager.cs
+++ VRTest/Assets/Scripts/UI_Manager.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
-    private Ray ray;
-    private RaycastHit hit;
+    //private Ray ray;
+    //private RaycastHit hit;
+    [SerializeField] private Camera camera;
 
     //[SerializeField]private Camera camera;
 
@@ -19,16 +20,27 @@ public class UI_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ray = Camera.main.ViewportPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
-    
-        if (Physics.Raycast(ray, out hit))
-        {
-            Debug.Log("testing");
-            Debug.Log(hit.collider.name);
-        }
+        //ray = camera.ViewportPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
+        // // camera.transform.position
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    Debug.Log("testing");
+        //    Debug.Log(hit.collider.name);
+        //}
 
-        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        //Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        Graze();
     }
 
-    
+    void Graze()
+    {
+        Debug.DrawRay(camera.transform.position, camera.transform.up * 5, Color.blue);
+        RaycastHit2D hit = Physics2D.Raycast(camera.transform.position, camera.transform.up, 5);
+        //Debug.Log(hit.collider.name);
+        if (hit.collider)
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+
 }

ef633dac86b05ad25e510a7802de9e0fa3b85ad1
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2019-10-31 17:40:40 -0400

    World UI added

---
+++ VRTest/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+
+    //[SerializeField]private Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ViewportPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
+    
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("testing");
+            Debug.Log(hit.collider.name);
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+    }
+
+    
+}
